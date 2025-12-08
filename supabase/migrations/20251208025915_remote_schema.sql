6aca9cff6e9b4942783e557acb2c4c9e4f9f4f93
Cristian <cristiancaraballo112@gmail.com>
2025-12-08 19:56:32 -0400

    [dx] splash migrations

--- supabase/migrations/20251208025915_remote_schema.sql
+++
@@ -1,7 +0,0 @@
-create extension if not exists "pg_cron" with schema "pg_catalog";
-
-create extension if not exists "pg_net" with schema "extensions";
-
-create schema if not exists "pgmq";
-
-create extension if not exists "pgmq" with schema "pgmq";
